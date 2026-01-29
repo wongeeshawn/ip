da486edcff24bae2a3b2088c538080bb271e6347
wongeeshawn <e1399244@u.nus.edu>
2026-01-29 22:56:01 +0800

    Level 2 add,list

--- src/main/java/Chat.java
+++ src/main/java/Chat.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Chat {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Chat");
@@ -14,11 +16,21 @@ public class Chat {
             input = scanner.nextLine();
 
             System.out.println("    ____________________________________________________________");
-            System.out.println("     " + input);
-            System.out.println("    ____________________________________________________________\n");
 
             if (input.equals("bye")) {
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println("    ____________________________________________________________\n");
                 break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("    ____________________________________________________________\n");
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("     added: " + input);
+                System.out.println("    ____________________________________________________________\n");
             }
         }
 
